38c315512aa841c8fafec90faffc045128752122
kim-youngji <80434135+kim-youngji@users.noreply.github.com>
2021-04-02 01:32:40 +0900

    Add files via upload

---
+++ kim car.cpp
@@ -0,0 +1,29 @@
+﻿class Car {
+	const string model;
+	bool power;
+	double speed;
+public:
+	Car(string m, bool p = false, double s = 0.0) :model(m), power(p), speed(s) {};
+	void set_power();
+	void set_speed(bool);
+	double get_speed() const
+};
+void Car::set_power()
+{
+	power = !power;
+	return
+}
+void Car::set_speed(bool a)
+{
+	if (!power)return
+		if (a)
+			speed += 0.1;
+		else
+			speed -= 0.1;
+	return
+}
+double Car::get_speed() const
+{
+	return speed;
+}
+
