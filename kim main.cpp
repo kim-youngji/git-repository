cf6704ae27847bd722219ab191498ddc7eb5582d
kim-youngji <80434135+kim-youngji@users.noreply.github.com>
2021-04-02 01:32:53 +0900

    Add files via upload

---
+++ kim main.cpp
@@ -0,0 +1,28 @@
+﻿int main()
+{
+	Car car1("t-roc");
+	car1.set_power();
+	car1.set_speed();
+	cout << car1.get_speed() << endl;
+	return 0;
+
+	Car car2("tiguan");
+	car2.set_power();
+	car2.set_speed();
+	cout << car2.get_speed() << endl;
+	return 0;
+
+	Car car3("jetta");
+	car3.set_power();
+	car3.set_speed();
+	cout << car3.get_speed() << endl;
+	return 0;
+
+	Car car4("arteon");
+	car4.set_power();
+	car4.set_speed();
+	cout << car4.get_speed() << endl;
+	return 0;
+
+
+}
